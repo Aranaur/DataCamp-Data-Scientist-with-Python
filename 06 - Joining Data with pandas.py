9cf091019970dd95c2320399c906c12db382aa5e
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-05-28 21:07:21 +0300

    add read

--- 06 - Joining Data with pandas.py
+++ 06 - Joining Data with pandas.py
@@ -1,4 +1,18 @@
 import pandas as pd
+pd.set_option('display.max_columns', 5)
 
-print('Hello!')
+# 1. Inner join
+census = pd.read_pickle('data/06/census.p')
+wards = pd.read_pickle('data/06/ward.p')
 
+census.head()
+census.shape
+
+wards.head()
+wards.shape
+
+wards_census = wards.merge(census, on="ward")
+wards_census.head()
+wards_census.shape
+
+wards_census.columns

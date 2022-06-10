2304a8b4fb2e0e03ff40739a9276943f3e1ecf21
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-06-10 12:59:00 +0300

    Inner join - taxi

--- 06 - Joining Data with pandas.py
+++ 06 - Joining Data with pandas.py
@@ -1,7 +1,7 @@
 import pandas as pd
 pd.set_option('display.max_columns', 5)
 
-# 1. Inner join
+#%%  1. Inner join
 census = pd.read_pickle('data/06/census.p')
 wards = pd.read_pickle('data/06/ward.p')
 
@@ -16,3 +16,18 @@ wards_census.head()
 wards_census.shape
 
 wards_census.columns
+
+wards_census = wards.merge(census, on="ward", suffixes=('_ward', '_cen'))
+wards_census.head()
+wards_census.shape
+
+#%%  1.1 Inner join - taxi
+taxi_owners = pd.read_pickle('data/06/taxi_owners.p')
+taxi_veh = pd.read_pickle('data/06/taxi_vehicles.p')
+
+taxi_own_veh = taxi_owners.merge(taxi_veh, on='vid')
+taxi_own_veh = taxi_owners.merge(taxi_veh, on='vid', suffixes=('_own', '_veh'))
+
+taxi_own_veh['fuel_type'].value_counts()
+
+#%%  One-to-many relationships

6c876fa4ee6e7e29ca38272e87dc4e70f2abda24
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-06-17 00:04:15 +0300

    fin

--- 07 - The GitHub History of the Scala Language.py
+++ 07 - The GitHub History of the Scala Language.py
@@ -1,4 +1,4 @@
-#%% 1. Scala's real-world project repository data
+# %% 1. Scala's real-world project repository data
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,7 +7,7 @@ pulls_one = pd.read_csv('data/07/pulls_2011-2013.csv')
 pulls_two = pd.read_csv('data/07/pulls_2014-2018.csv')
 pull_files = pd.read_csv('data/07/pull_files.csv')
 
-#%% 2. Preparing and cleaning the data
+# %% 2. Preparing and cleaning the data
 
 # Append pulls_one to pulls_two
 pulls = pd.concat([pulls_one, pulls_two], ignore_index=True)
@@ -15,12 +15,12 @@ pulls = pd.concat([pulls_one, pulls_two], ignore_index=True)
 # Convert the date for the pulls object
 pulls['date'] = pd.to_datetime(pulls['date'], utc=True)
 
-#%% 3. Merging the DataFrames
+# %% 3. Merging the DataFrames
 
 # Merge the two DataFrames
 data = pulls.merge(pull_files, on='pid')
 
-#%% 4. Is the project still actively maintained?
+# %% 4. Is the project still actively maintained?
 
 # Create a column that will store the month
 data['month'] = data['date'].dt.month
@@ -36,7 +36,7 @@ counts.plot(kind='bar', figsize=(12, 4))
 plt.show()
 
 
-#%% 5. Is there camaraderie in the project?
+# %% 5. Is there camaraderie in the project?
 
 # Group by the submitter
 by_user = data.groupby('user')['pid'].count()
@@ -45,7 +45,7 @@ by_user = data.groupby('user')['pid'].count()
 by_user.plot(kind='hist', figsize=(12, 4))
 plt.show()
 
-#%% 6. What files were changed in the last ten pull requests?
+# %% 6. What files were changed in the last ten pull requests?
 
 # Identify the last 10 pull requests
 last_10 = pulls.nlargest(10, 'date')
@@ -59,7 +59,7 @@ files = set(joined_pr['file'])
 # Print the results
 files
 
-#%% 7. Who made the most pull requests to a given file?
+# %% 7. Who made the most pull requests to a given file?
 
 # This is the file we are interested in:
 file = 'src/compiler/scala/reflect/reify/phases/Calculate.scala'
@@ -73,7 +73,7 @@ author_counts = file_pr.groupby('user')['pid'].count()
 # Print the top 3 developers
 author_counts.nlargest(3)
 
-#%% 8. Who made the last ten pull requests on a given file?
+# %% 8. Who made the last ten pull requests on a given file?
 
 file = 'src/compiler/scala/reflect/reify/phases/Calculate.scala'
 
@@ -88,3 +88,42 @@ users_last_10 = set(joined_pr.nlargest(10, 'date')['user'])
 
 # Printing the results
 users_last_10
+
+# %% 9. The pull requests of two special developers
+
+# The developers we are interested in
+authors = ['xeno-by', 'soc']
+
+# Get all the developers' pull requests
+by_author = pulls[pulls['user'].isin(authors)]
+
+# Count the number of pull requests submitted each year
+counts = by_author.groupby([by_author['user'], by_author['date'].dt.year]).agg({'pid': 'count'}).reset_index()
+
+# Convert the table to a wide format
+counts_wide = counts.pivot_table(index='date', columns='user', values='pid', fill_value=0)
+
+# Plot the results
+counts_wide.plot(kind="bar")
+plt.show()
+
+# %% 10. Visualizing the contributions of each developer
+
+authors = ['xeno-by', 'soc']
+file = 'src/compiler/scala/reflect/reify/phases/Calculate.scala'
+
+# Select the pull requests submitted by the authors, from the `data` DataFrame
+by_author = data[data['user'].isin(authors)]
+
+# Select the pull requests that affect the file
+by_file = by_author[by_author["file"] == file]
+
+# Group and count the number of PRs done by each user each year
+grouped = by_file.groupby(['user', by_file['date'].dt.year]).count()['pid'].reset_index()
+
+# Transform the data into a wide format
+by_file_wide = grouped.pivot_table(index='date', columns='user', values='pid', fill_value=0)
+
+# Plot the results
+by_file_wide.plot(kind='bar')
+plt.show()

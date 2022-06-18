3f87e8884686f20cac17b4a9c3c804bb207e93a3
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-06-18 17:24:16 +0300

    2.1

--- 08 - Introduction to Data Visualization with Matplotlib.py
+++ 08 - Introduction to Data Visualization with Matplotlib.py
@@ -105,3 +105,62 @@ fig, ax = plt.subplots(2, 1, sharey=True)
 
 # %% 2. Plotting time-series data
 
+climate_change
+
+climate_change.index
+
+climate_change['co2']
+
+fig, ax = plt.subplots()
+ax.plot(climate_change.index, climate_change['co2'])
+ax.set_xlabel('Time')
+ax.set_ylabel('CO2 (ppm)')
+plt.show()
+
+sixties = climate_change['1960-01-01':'1969-12-31']
+
+fig, ax = plt.subplots()
+ax.plot(sixties.index, sixties['co2'])
+ax.set_xlabel('Time')
+ax.set_ylabel('CO2 (ppm)')
+plt.show()
+
+sixty_nine = climate_change['1969-01-01':'1969-12-31']
+
+fig, ax = plt.subplots()
+ax.plot(sixty_nine.index, sixty_nine['co2'])
+ax.set_xlabel('Time')
+ax.set_ylabel('CO2 (ppm)')
+plt.show()
+
+# %%
+
+# Add the time-series for "relative_temp" to the plot
+ax.plot(climate_change.index, climate_change['relative_temp'])
+
+# Set the x-axis label
+ax.set_xlabel('Time')
+
+# Set the y-axis label
+ax.set_ylabel('Relative temperature (Celsius)')
+
+# Show the figure
+plt.show()
+
+# %%
+
+# Use plt.subplots to create fig and ax
+fig, ax = plt.subplots()
+
+# Create variable seventies with data from "1970-01-01" to "1979-12-31"
+seventies = climate_change['1970-01-01':'1979-12-31']
+
+# Add the time-series for "co2" data from seventies to the plot
+ax.plot(seventies.index, seventies['co2'])
+
+# Show the figure
+plt.show()
+
+# %% 2.1 Plotting time-series with different variables
+
+

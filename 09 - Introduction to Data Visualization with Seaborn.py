66c20b65c16f8684842e96f6b3ee0b001cd3c54d
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-06-22 12:57:17 +0300

    start sns

--- 09 - Introduction to Data Visualization with Seaborn.py
+++ 09 - Introduction to Data Visualization with Seaborn.py
@@ -16,3 +16,30 @@ survey.head()
 
 # %% 1. Introduction to Seaborn
 
+height = [62, 64, 69, 75, 66, 68, 65, 71, 76, 73]
+weight = [120, 136, 148, 175, 137, 165, 154, 172, 200, 187]
+
+sns.scatterplot(x=height, y=weight)
+plt.show()
+
+gender = ['Female', 'Female', 'Female', 'Female', 'Male', 'Male', 'Male', 'Male', 'Male', 'Male']
+
+sns.countplot(x=gender)
+plt.show()
+
+# %%
+country_data.columns
+
+gdp = country_data['GDP ($ per capita)']
+phones = country_data['Phones (per 1000)']
+percent_literate = country_data['Literacy (%)']
+region = country_data['Region']
+
+sns.scatterplot(x=gdp, y=phones)
+plt.show()
+
+sns.scatterplot(x=gdp, y=percent_literate)
+plt.show()
+
+sns.countplot(y=region)
+plt.show()

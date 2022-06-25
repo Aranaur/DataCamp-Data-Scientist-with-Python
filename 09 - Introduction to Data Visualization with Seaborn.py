b725cc21d345876a55d1ebcb064c7599cc95a3c4
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-06-25 13:07:07 +0300

    finish sns

--- 09 - Introduction to Data Visualization with Seaborn.py
+++ 09 - Introduction to Data Visualization with Seaborn.py
@@ -583,3 +583,291 @@ sns.catplot(x="romantic", y="absences",
 plt.show()
 
 # %% 4. Changing plot style and color
+
+# %% set_style
+# white, dark, whitegrid, darkgrid, ticks
+
+sns.set_style('ticks')
+sns.catplot(x="romantic", y="absences",
+            data=student_data,
+            kind="point",
+            hue="school",
+            ci=None)
+plt.show()
+
+# %% set_style
+# RdBu, PRGn, RdBu_r, PRGn_r
+# Greys, Blues, PuRd, GnBu
+
+category_order = ['<2 hours',
+                  '2 to 5 hours',
+                  '5 to 10 hours',
+                  '>10 hours']
+
+sns.set_palette('RdBu')
+sns.catplot(x='study_time',
+            data=student_data,
+            kind='count',
+            order=category_order)
+plt.show()
+
+# %% custom set_style
+
+custom_palette = ['red', 'green', 'orange', 'blue', 'yellow', 'purple']
+sns.set_palette(custom_palette)
+sns.catplot(x='study_time',
+            data=student_data,
+            kind='count',
+            order=category_order)
+plt.show()
+
+# %% custom set_style
+
+custom_palette = ['#FBB4AE', '#B3CDE3', '#CCEBC5', '#DECBE4', '#FED9A6']
+sns.set_palette(custom_palette)
+sns.catplot(x='study_time',
+            data=student_data,
+            kind='count',
+            order=category_order)
+plt.show()
+
+# %% set_context()
+# paper, notebook, talk, poster
+
+sns.set_context('notebook')
+sns.catplot(x='study_time',
+            data=student_data,
+            kind='count',
+            order=category_order)
+plt.show()
+
+# %%
+
+# Set the style to "whitegrid"
+sns.set_style('whitegrid')
+# Create a count plot of survey responses
+category_order = ["Never", "Rarely", "Sometimes",
+                  "Often", "Always"]
+sns.catplot(x="Parents Advice",
+            data=survey_data,
+            kind="count",
+            order=category_order)
+# Show plot
+plt.show()
+
+# %%
+
+# Set the color palette to "Purples"
+sns.set_style("whitegrid")
+sns.set_palette("Purples")
+# Create a count plot of survey responses
+category_order = ["Never", "Rarely", "Sometimes",
+                  "Often", "Always"]
+sns.catplot(x="Parents Advice",
+            data=survey_data,
+            kind="count",
+            order=category_order)
+# Show plot
+plt.show()
+
+# %%
+
+# Change the color palette to "RdBu"
+sns.set_style("whitegrid")
+sns.set_palette("RdBu")
+# Create a count plot of survey responses
+category_order = ["Never", "Rarely", "Sometimes",
+                  "Often", "Always"]
+sns.catplot(x="Parents Advice",
+            data=survey_data,
+            kind="count",
+            order=category_order)
+# Show plot
+plt.show()
+
+# %%
+
+# Set the context to "paper"
+sns.set_context('paper')
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+# Show plot
+plt.show()
+
+# %%
+
+# Change the context to "notebook"
+sns.set_context("notebook")
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+# Show plot
+plt.show()
+
+# %%
+
+# Change the context to "talk"
+sns.set_context("talk")
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+# Show plot
+plt.show()
+
+# %%
+
+# Change the context to "poster"
+sns.set_context("poster")
+# Create bar plot
+sns.catplot(x="Number of Siblings", y="Feels Lonely",
+            data=survey_data, kind="bar")
+# Show plot
+plt.show()
+
+# %%
+
+# Set the style to "darkgrid"
+sns.set_style('darkgrid')
+# Set a custom color palette
+custom_palette = ['#39A7D0', '#36ADA4']
+sns.set_palette(custom_palette)
+# Create the box plot of age distribution by gender
+sns.catplot(x="Gender", y="Age",
+            data=survey_data, kind="box")
+# Show plot
+plt.show()
+
+# %% 4.1 Adding titles and labels: Part 1
+
+# %%
+
+g = sns.scatterplot(x='Region', y='Birthrate', data=country_data)
+type(g)
+
+g = sns.catplot(x='Region', y='Birthrate', data=country_data, kind='box')
+g.fig.suptitle('New Title', y=1.03)
+plt.show()
+
+# %%
+
+# Create scatter plot
+g = sns.relplot(x="weight",
+                y="horsepower",
+                data=mpg,
+                kind="scatter")
+# Identify plot type
+type_of_g = type(g)
+# Print type
+print(type_of_g)
+
+# %%
+
+# Create scatter plot
+g = sns.relplot(x="weight",
+                y="horsepower",
+                data=mpg,
+                kind="scatter")
+# Add a title "Car Weight vs. Horsepower"
+g.fig.suptitle('Car Weight vs. Horsepower')
+# Show plot
+plt.show()
+
+# %% 4.2 Adding titles and labels: Part 2
+# %%
+
+# 1
+g = sns.catplot(x='Region', y='Birthrate', data=country_data, kind='box')
+g.fig.suptitle('New Title', y=1.03)
+plt.show()
+
+# 2
+g = sns.boxplot(x='Region', y='Birthrate', data=country_data)
+g.set_title('New Title', y=1.03)
+plt.show()
+
+# %%
+
+g = sns.catplot(x='Region', y='Birthrate', data=country_data, kind='box', col='Climate')
+g.fig.suptitle('New Title', y=1.03)
+g.set_titles('This is {col_name}')
+
+# %%
+
+g = sns.catplot(x='Region', y='Birthrate', data=country_data, kind='box')
+g.set(xlabel='New X Label',
+      ylabel='New Y Label')
+plt.show()
+
+# %%
+
+g = sns.catplot(x='Region', y='Birthrate', data=country_data, kind='box')
+plt.xticks(rotation=90)
+plt.show()
+
+# %%
+
+# Create line plot
+g = sns.lineplot(x="model_year", y="mpg_mean",
+                 data=mpg_mean,
+                 hue="origin")
+# Add a title "Average MPG Over Time"
+g.set_title('Average MPG Over Time')
+# Show plot
+plt.show()
+
+# %%
+
+# Create line plot
+g = sns.lineplot(x="model_year", y="mpg_mean",
+                 data=mpg_mean,
+                 hue="origin")
+# Add a title "Average MPG Over Time"
+g.set_title("Average MPG Over Time")
+# Add x-axis and y-axis labels
+g.set(xlabel='Car Model Year',
+      ylabel='Average MPG')
+# Show plot
+plt.show()
+
+# %%
+
+# Create point plot
+sns.catplot(x="origin",
+            y="acceleration",
+            data=mpg,
+            kind="point",
+            join=False,
+            capsize=0.1)
+# Rotate x-tick labels
+plt.xticks(rotation=90)
+# Show plot
+plt.show()
+
+# %% 4.3 Putting it all together
+
+# Set palette to "Blues"
+sns.set_palette('Blues')
+# Adjust to add subgroups based on "Interested in Pets"
+g = sns.catplot(x="Gender",
+                y="Age", data=survey_data,
+                kind="box", hue="Interested in Pets")
+# Set title to "Age of Those Interested in Pets vs. Not"
+g.fig.suptitle('Age of Those Interested in Pets vs. Not')
+# Show plot
+plt.show()
+
+# %%
+
+# Set the figure style to "dark"
+sns.set_style('dark')
+# Adjust to add subplots per gender
+g = sns.catplot(x="Village - town", y="Likes Techno",
+                data=survey_data, kind="bar",
+                col='Gender')
+# Add title and axis labels
+g.fig.suptitle("Percentage of Young People Who Like Techno", y=1.02)
+g.set(xlabel="Location of Residence",
+      ylabel="% Who Like Techno")
+# Show plot
+plt.show()

c338a109ea9ad39da7a93e036c41422487aac297
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-06-29 14:01:34 +0300

    start 1

---
+++ 11 - Python Data Science Toolbox (Part 1).py
@@ -0,0 +1,14 @@
+# Importing course packages; you can add more too!
+import pandas as pd
+from functools import reduce
+
+# Importing course datasets as DataFrames
+tweets = pd.read_csv('data/11/tweets.csv')
+
+tweets.head()
+
+#%% 1. Writing your own functions
+
+#%% 1.1 User-defined functions
+
+

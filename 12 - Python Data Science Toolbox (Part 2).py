ae398d35615f238bfe778a2cb2adf067c09a85aa
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-05 00:15:19 +0300

    start

---
+++ 12 - Python Data Science Toolbox (Part 2).py
@@ -0,0 +1,14 @@
+#%% Import the course packages
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#%% Import the course datasets as DataFrames
+world_ind = pd.read_csv('data/12/world_ind_pop_data.csv')
+tweets = pd.read_csv('data/12/tweets.csv')
+
+# Preview the first DataFrame
+world_ind
+
+#%% 1. Using iterators in PythonLand
+
+#%% 1.1 Introduction to iterators

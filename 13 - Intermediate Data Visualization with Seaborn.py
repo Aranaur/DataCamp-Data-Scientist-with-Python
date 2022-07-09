06927bcac8823f868114433b278f04acefbedc8b
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-09 16:37:16 +0300

    fin 1.1

--- 13 - Intermediate Data Visualization with Seaborn.py
+++ 13 - Intermediate Data Visualization with Seaborn.py
@@ -16,3 +16,45 @@ bike_share.head()  # Display the first five rows of this DataFrame
 #%% 1. Seaborn Introduction
 
 #%% 1.1 Introduction to Seaborn
+
+fig, ax = plt.subplots()
+ax.hist(bike_share['temp'])
+
+bike_share['temp'].plot.hist()
+
+sns.histplot(bike_share['temp'])
+
+sns.displot(bike_share['temp'], kind='kde')
+
+#%%
+
+grant_file = 'https://assets.datacamp.com/production/course_7030/datasets/schoolimprovement2010grants.csv'
+
+# import all modules
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Read in the DataFrame
+df = pd.read_csv(grant_file)
+
+#%%
+
+# Display pandas histogram
+df['Award_Amount'].plot.hist()
+plt.show()
+
+# Clear out the pandas histogram
+plt.clf()
+
+#%%
+
+# Display a Seaborn displot
+sns.displot(df['Award_Amount'])
+plt.show()
+
+# Clear the displot
+plt.clf()
+
+#%% 1.2 Using the distribution plot
+

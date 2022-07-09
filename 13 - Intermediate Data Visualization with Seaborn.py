81f69e405401f9909732b104e6af22c018e23727
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-09 20:34:25 +0300

    start 2

--- 13 - Intermediate Data Visualization with Seaborn.py
+++ 13 - Intermediate Data Visualization with Seaborn.py
@@ -58,3 +58,79 @@ plt.clf()
 
 #%% 1.2 Using the distribution plot
 
+sns.displot(df['Award_Amount'], kde=True, bins=10)
+
+sns.displot(df['Award_Amount'], kind='kde', rug=True, fill=True)
+
+sns.displot(df['Award_Amount'], kind='ecdf', rug=True)
+
+#%%
+
+# Create a displot
+sns.displot(df['Award_Amount'],
+            bins=20)
+
+# Display the plot
+plt.show()
+
+#%%
+
+# Create a displot of the Award Amount
+sns.displot(df['Award_Amount'],
+            kind='kde',
+            rug=True,
+            fill=True)
+
+# Plot the results
+plt.show()
+
+#%% 1.3 Regression Plots in Seaborn
+
+sns.regplot(data=bike_share, x='temp', y='atemp')
+sns.regplot(data=bike_share, x='temp', y='atemp')
+
+sns.lmplot(data=bike_share, x='temp', y='atemp')
+sns.lmplot(data=bike_share, x='temp', y='atemp', hue='mnth')
+sns.lmplot(data=bike_share, x='temp', y='atemp', col='holiday')
+
+#%%
+
+# Create a regression plot of premiums vs. insurance_losses
+sns.regplot(data=insurance, x='insurance_losses', y='premiums')
+
+# Display the plot
+plt.show()
+
+#%%
+
+# Create an lmplot of premiums vs. insurance_losses
+sns.lmplot(data=insurance, x='insurance_losses', y='premiums')
+
+# Display the second plot
+plt.show()
+
+#%%
+
+# Create a regression plot using hue
+sns.lmplot(data=insurance,
+           x="insurance_losses",
+           y="premiums",
+           hue="Region")
+
+# Show the results
+plt.show()
+
+#%%
+
+# Create a regression plot with multiple rows
+sns.lmplot(data=insurance,
+           x="insurance_losses",
+           y="premiums",
+           row="Region")
+
+# Show the plot
+plt.show()
+
+#%% 2. Customizing Seaborn Plots
+
+#%% 2.1 Using Seaborn Styles

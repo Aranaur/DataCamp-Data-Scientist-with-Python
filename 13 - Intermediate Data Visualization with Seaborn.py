b3da2603792a6bd21217659865c27c0a168d15aa
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-11 01:11:56 +0300

    start 3.1

--- 13 - Intermediate Data Visualization with Seaborn.py
+++ 13 - Intermediate Data Visualization with Seaborn.py
@@ -366,3 +366,24 @@ sns.pointplot(data=grants,
 sns.countplot(data=grants,
               y='Region',
               hue='Model Selected')
+#%%
+
+# Create the stripplot
+sns.stripplot(data=df,
+              x='Award_Amount',
+              y='Model Selected',
+              jitter=True)
+
+plt.show()
+
+#%%
+
+# Create and display a swarmplot with hue set to the Region
+sns.swarmplot(data=df,
+              x='Award_Amount',
+              y='Model Selected',
+              hue='Region')
+
+plt.show()
+
+#%%

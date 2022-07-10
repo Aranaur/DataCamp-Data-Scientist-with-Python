1ccc01b9606450d4e067395f9b2c9407333f83ef
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-10 12:24:30 +0300

    start 3

--- 13 - Intermediate Data Visualization with Seaborn.py
+++ 13 - Intermediate Data Visualization with Seaborn.py
@@ -134,3 +134,200 @@ plt.show()
 #%% 2. Customizing Seaborn Plots
 
 #%% 2.1 Using Seaborn Styles
+
+sns.set()
+insurance['insurance_losses'].plot.hist()
+
+for style in ['white', 'dark', 'whitegrid', 'darkgrid']:
+    sns.set_style(style)
+    sns.displot(insurance['insurance_losses'])
+    plt.show()
+
+sns.set_style('white')
+sns.displot(insurance['insurance_losses'])
+sns.despine(left=True)
+
+#%%
+
+# Plot the pandas histogram
+df['fmr_2'].plot.hist()
+plt.show()
+plt.clf()
+
+# Set the default seaborn style
+sns.set()
+
+# Plot the pandas histogram again
+df['fmr_2'].plot.hist()
+plt.show()
+plt.clf()
+
+#%%
+
+sns.set_style('dark')
+sns.displot(df['fmr_2'])
+plt.show()
+plt.clf()
+
+#%%
+
+sns.set_style('whitegrid')
+sns.displot(df['fmr_2'])
+plt.show()
+plt.clf()
+
+#%%
+
+# Set the style to white
+sns.set_style('white')
+
+# Create a regression plot
+sns.lmplot(data=df,
+           x='pop2010',
+           y='fmr_2')
+
+# Remove the spines
+sns.despine(top=True, right=True)
+
+# Show the plot and clear the figure
+plt.show()
+plt.clf()
+
+#%% 2.2 Colors in Seaborn
+
+sns.set(color_codes=True)
+sns.displot(insurance['insurance_losses'], color='g')
+
+palettes = ['deep', 'muted', 'pastel', 'bright', 'dark', 'colorblind']
+for p in palettes:
+    sns.set_palette(p)
+    sns.displot(insurance['insurance_losses'])
+
+palettes = ['deep', 'muted', 'pastel', 'bright', 'dark', 'colorblind']
+for p in palettes:
+    sns.set_palette(p)
+    sns.palplot(sns.color_palette())
+    plt.show()
+
+sns.palplot(sns.color_palette('Paired', 10))
+
+sns.palplot(sns.color_palette('Blues', 10))
+
+sns.palplot(sns.color_palette('BrBG', 10))
+
+#%%
+
+# Set style, enable color code, and create a magenta displot
+sns.set(color_codes=True)
+sns.displot(df['fmr_3'], color='m')
+
+# Show the plot
+plt.show()
+
+#%%
+
+# Loop through differences between bright and colorblind palettes
+for p in ['bright', 'colorblind']:
+    sns.set_palette(p)
+    sns.displot(df['fmr_3'])
+    plt.show()
+
+    # Clear the plots
+    plt.clf()
+
+#%%
+
+sns.palplot(sns.color_palette('Purples', 8))
+plt.show()
+
+sns.palplot(sns.color_palette('husl', 10))
+plt.show()
+
+sns.palplot(sns.color_palette('coolwarm', 6))
+plt.show()
+
+#%% 2.3 Customizing with matplotlib
+
+fig, ax = plt.subplots()
+sns.histplot(insurance['insurance_losses'], ax=ax)
+ax.set(xlabel='Insurance Losses')
+
+fig, ax = plt.subplots()
+sns.histplot(insurance['insurance_losses'], ax=ax)
+ax.set(xlabel='Insurance Losses',
+       ylabel='Distribution', xlim=(0, 250),
+       title='Insurance Losses and Distribution')
+
+fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(7, 4))
+sns.histplot(grants['Award_Amount'], stat='density', ax=ax0)
+sns.histplot(grants.query('State == "AK"')['Award_Amount'], stat='density', ax=ax1)
+ax1.set(xlabel='Tuition (NM)', xlim=(0, 70000))
+ax1.axvline(x=20000, label='My Budget', linestyle='--')
+ax1.legend
+
+#%%
+
+# Create a figure and axes
+fig, ax = plt.subplots()
+
+# Plot the distribution of data
+sns.histplot(df['fmr_3'], ax=ax)
+
+# Create a more descriptive x axis label
+ax.set(xlabel="3 Bedroom Fair Market Rent")
+
+# Show the plot
+plt.show()
+
+#%%
+
+# Create a figure and axes
+fig, ax = plt.subplots()
+
+# Plot the distribution of 1 bedroom rents
+sns.histplot(df['fmr_1'], ax=ax)
+
+# Modify the properties of the plot
+ax.set(xlabel="1 Bedroom Fair Market Rent",
+       xlim=(100,1500),
+       title="US Rent")
+
+# Display the plot
+plt.show()
+
+#%%
+
+# Create a figure and axes. Then plot the data
+fig, ax = plt.subplots()
+sns.histplot(df['fmr_1'], ax=ax)
+
+# Customize the labels and limits
+ax.set(xlabel="1 Bedroom Fair Market Rent", xlim=(100,1500), title="US Rent")
+
+# Add vertical lines for the median and mean
+ax.axvline(x=median, color='m', label='Median', linestyle='--', linewidth=2)
+ax.axvline(x=mean, color='b', label='Mean', linestyle='-', linewidth=2)
+
+# Show the legend and plot the data
+ax.legend()
+plt.show()
+
+#%%
+
+# Create a plot with 1 row and 2 columns that share the y axis label
+fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, sharey=True)
+
+# Plot the distribution of 1 bedroom apartments on ax0
+sns.histplot(df['fmr_1'], ax=ax0)
+ax0.set(xlabel="1 Bedroom Fair Market Rent", xlim=(100,1500))
+
+# Plot the distribution of 2 bedroom apartments on ax1
+sns.histplot(df['fmr_2'], ax=ax1)
+ax1.set(xlabel="2 Bedroom Fair Market Rent", xlim=(100,1500))
+
+# Display the plot
+plt.show()
+
+#%% 3. Additional Plot Types
+
+#%% 3.1 Categorical Plot Types

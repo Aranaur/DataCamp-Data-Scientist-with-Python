d6be2241791b11b77ac0548e62bc6224867197cb
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-13 01:04:05 +0300

    finish

--- 13 - Intermediate Data Visualization with Seaborn.py
+++ 13 - Intermediate Data Visualization with Seaborn.py
@@ -575,3 +575,307 @@ plt.show()
 plt.clf()
 
 #%% 3.3 Matrix plots
+
+df_crosstab = pd.crosstab(bike_share['mnth'], bike_share['weekday'],
+            values=bike_share['total_rentals'], aggfunc='mean').round(0)
+
+sns.heatmap(df_crosstab)
+
+sns.heatmap(df_crosstab, annot=True, fmt="1", cmap='YlGnBu', cbar=False, linewidths=0.5)
+
+sns.heatmap(df_crosstab, annot=True, fmt="1", cmap='YlGnBu', cbar=True, center=df_crosstab.loc[9, 6])
+
+cols = ['total_rentals', 'temp', 'casual', 'hum', 'windspeed']
+sns.heatmap(bike_share[cols].corr(), cmap='YlGnBu')
+
+#%%
+# Create a crosstab table of the data
+pd_crosstab = pd.crosstab(df["Group"], df["YEAR"])
+print(pd_crosstab)
+
+# Plot a heatmap of the table
+sns.heatmap(pd_crosstab)
+
+# Rotate tick marks for visibility
+plt.yticks(rotation=0)
+plt.xticks(rotation=90)
+
+plt.show()
+
+#%%
+# Create the crosstab DataFrame
+pd_crosstab = pd.crosstab(df["Group"], df["YEAR"])
+
+# Plot a heatmap of the table with no color bar and using the BuGn palette
+sns.heatmap(pd_crosstab, cbar=False, cmap="BuGn", linewidths=0.3)
+
+# Rotate tick marks for visibility
+plt.yticks(rotation=0)
+plt.xticks(rotation=90)
+
+#Show the plot
+plt.show()
+plt.clf()
+
+#%% 4 Creating Plots on Data Aware Grids
+
+#%% 4.1 Using FacetGrid, catplot and lmplot
+
+g = sns.FacetGrid(college_data, col='HIGHDEG')
+g.map(sns.boxplot, 'Tuition', order=['1', '2', '3', '4'])
+
+sns.catplot(x='Tuition', data=college_data, col='HIGHDEG', kind='box')
+
+g = sns.FacetGrid(college_data, col='HIGHDEG')
+g.map(plt.scatter, 'Tuition', 'SAT_AVG_ALL')
+
+sns.lmplot(data=college_data, x='Tuition', y='SAT_AVG_ALL', col='HIGHDEG', fit_reg=False)
+
+sns.lmplot(data=college_data, x='Tuition', y='SAT_AVG_ALL', col='HIGHDEG', row='REGION')
+
+#%%
+# Create FacetGrid with Degree_Type and specify the order of the rows using row_order
+g2 = sns.FacetGrid(df,
+                   row="Degree_Type",
+                   row_order=['Graduate', 'Bachelors', 'Associates', 'Certificate'])
+
+# Map a pointplot of SAT_AVG_ALL onto the grid
+g2.map(sns.pointplot, 'SAT_AVG_ALL')
+
+# Show the plot
+plt.show()
+plt.clf()
+
+#%%
+# Create a factor plot that contains boxplots of Tuition values
+sns.catplot(data=df,
+            x='Tuition',
+            kind='box',
+            row='Degree_Type')
+
+plt.show()
+plt.clf()
+
+#%%
+# Create a facetted pointplot of Average SAT_AVG_ALL scores facetted by Degree Type
+sns.catplot(data=df,
+            x='SAT_AVG_ALL',
+            kind='point',
+            row='Degree_Type',
+            row_order=['Graduate', 'Bachelors', 'Associates', 'Certificate'])
+
+plt.show()
+plt.clf()
+
+#%%
+# Create a FacetGrid varying by column and columns ordered with the degree_order variable
+g = sns.FacetGrid(df, col="Degree_Type", col_order=degree_ord)
+
+# Map a scatter plot of Undergrad Population compared to PCTPELL
+g.map(plt.scatter, 'UG', 'PCTPELL')
+
+plt.show()
+plt.clf()
+
+#%%
+# Re-create the previous plot as an lmplot
+sns.lmplot(data=df,
+           x='UG',
+           y='PCTPELL',
+           col="Degree_Type",
+           col_order=degree_ord)
+
+plt.show()
+plt.clf()
+
+#%%
+# Create an lmplot that has a column for Ownership, a row for Degree_Type and hue based on the WOMENONLY column
+sns.lmplot(data=df,
+           x='SAT_AVG_ALL',
+           y='Tuition',
+           col="Ownership",
+           row='Degree_Type',
+           row_order=['Graduate', 'Bachelors'],
+           hue='WOMENONLY',
+           col_order=inst_ord)
+
+plt.show()
+plt.clf()
+
+#%% 4.2 Using PairGrid and pairplot
+
+g = sns.PairGrid(college_data, vars=['PCTPELL', 'PCTFLOAN'])
+g = g.map_diag(sns.histplot)
+g = g.map_offdiag(sns.scatterplot)
+
+sns.pairplot(college_data, vars=['PCTPELL', 'PCTFLOAN'], kind='reg', diag_kind='hist')
+
+sns.pairplot(college_data.query("REGION < 3"),
+             vars=['PCTPELL', 'PCTFLOAN', 'UG'],
+             hue='REGION', palette='husl',
+             plot_kws={'alpha': 0.5})
+
+#%%
+# Create a PairGrid with a scatter plot for fatal_collisions and premiums
+g = sns.PairGrid(df, vars=["fatal_collisions", "premiums"])
+g2 = g.map(sns.scatterplot)
+
+plt.show()
+plt.clf()
+
+#%%
+# Create the same PairGrid but map a histogram on the diag
+g = sns.PairGrid(df, vars=["fatal_collisions", "premiums"])
+g2 = g.map_diag(sns.histplot)
+g3 = g2.map_offdiag(sns.scatterplot)
+
+plt.show()
+plt.clf()
+
+#%%
+# Create a pairwise plot of the variables using a scatter plot
+sns.pairplot(data=df,
+             vars=["fatal_collisions", "premiums"],
+             kind='scatter')
+
+plt.show()
+plt.clf()
+
+#%%
+# Plot the same data but use a different color palette and color code by Region
+sns.pairplot(data=df,
+             vars=["fatal_collisions", "premiums"],
+             kind='scatter',
+             hue='Region',
+             palette='RdBu',
+             diag_kws={'alpha':.5})
+
+plt.show()
+plt.clf()
+
+#%%
+# Build a pairplot with different x and y variables
+sns.pairplot(data=df,
+             x=["fatal_collisions_speeding", "fatal_collisions_alc"],
+             y=['premiums', 'insurance_losses'],
+             kind='scatter',
+             hue='Region',
+             palette='husl')
+
+plt.show()
+plt.clf()
+
+#%%
+# Build a pairplot with different x and y variables
+sns.pairplot(data=df,
+             x_vars=["fatal_collisions_speeding", "fatal_collisions_alc"],
+             y_vars=['premiums', 'insurance_losses'],
+             kind='scatter',
+             hue='Region',
+             palette='husl')
+
+plt.show()
+plt.clf()
+
+#%%
+# plot relationships between insurance_losses and premiums
+sns.pairplot(data=df,
+             vars=["insurance_losses", "premiums"],
+             kind='reg',
+             palette='BrBG',
+             diag_kind = 'kde',
+             hue='Region')
+
+plt.show()
+plt.clf()
+
+#%% 4.2 Using JointGrid and jointplot
+
+g = sns.JointGrid(data=bike_share, x='temp', y='total_rentals', dropna=True)
+g.plot(sns.regplot, sns.histplot)
+
+g = sns.JointGrid(data=bike_share, x='temp', y='total_rentals', dropna=True)
+g = g.plot_joint(sns.kdeplot)
+g = g.plot_marginals(sns.kdeplot, shade=True)
+
+sns.jointplot(data=bike_share, x='temp', y='total_rentals', kind='hex')
+
+g = (sns.jointplot(x='temp',
+                   y='total_rentals',
+                   kind='scatter',
+                   data=bike_share.query("mnth == 12 & total_rentals > 1000"))
+     .plot_joint(sns.kdeplot))
+
+
+#%%
+# Build a JointGrid comparing humidity and total_rentals
+sns.set_style("whitegrid")
+g = sns.JointGrid(x="hum",
+                  y="total_rentals",
+                  data=df,
+                  xlim=(0.1, 1.0))
+
+g.plot(sns.regplot, sns.histplot)
+
+plt.show()
+plt.clf()
+
+#%%
+# Create a jointplot similar to the JointGrid
+sns.jointplot(x="hum",
+              y="total_rentals",
+              kind='reg',
+              data=df)
+
+plt.show()
+plt.clf()
+
+#%%
+# Plot temp vs. total_rentals as a regression plot
+sns.jointplot(x="temp",
+              y="total_rentals",
+              kind='reg',
+              data=df,
+              order=2,
+              xlim=(0, 1))
+
+plt.show()
+plt.clf()
+
+#%%
+# Plot a jointplot showing the residuals
+sns.jointplot(x="temp",
+              y="total_rentals",
+              kind='resid',
+              data=df,
+              order=2)
+
+plt.show()
+plt.clf()
+
+#%%
+# Create a jointplot of temp vs. casual riders
+# Include a kdeplot over the scatter plot
+g = sns.jointplot(x="temp",
+                  y="casual",
+                  kind='scatter',
+                  data=df,
+                  marginal_kws=dict(bins=10))
+g.plot_joint(sns.kdeplot)
+
+plt.show()
+plt.clf()
+
+#%%
+# Replicate the above plot but only for registered riders
+g = sns.jointplot(x="temp",
+                  y="registered",
+                  kind='scatter',
+                  data=df,
+                  marginal_kws=dict(bins=10))
+g.plot_joint(sns.kdeplot)
+
+plt.show()
+plt.clf()
+
+#%% 4.3 Selecting Seaborn Plots

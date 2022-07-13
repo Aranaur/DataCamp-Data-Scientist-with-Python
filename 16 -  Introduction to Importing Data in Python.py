54c332d19704bc2422ed3c5d3f0f7461d28cacac
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-13 19:59:28 +0300

    start

---
+++ 16 -  Introduction to Importing Data in Python.py
@@ -0,0 +1,31 @@
+# Import the course packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+import h5py
+from sas7bdat import SAS7BDAT
+from sqlalchemy import create_engine
+import pickle
+
+
+#%% 1. Introduction and flat files
+
+#%% 1.1 Welcome to the course!
+
+
+
+
+
+
+
+
+
+
+
+
+#%% 2. Importing data from other file types
+
+#%% 3. Working with relational databases in Python
+
+

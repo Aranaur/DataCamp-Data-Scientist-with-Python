cfeed7b840f057c6f536affa79f543bea904d3d3
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-17 23:25:38 +0300

    start 1

--- 16 -  Introduction to Importing Data in Python.py
+++ 16 -  Introduction to Importing Data in Python.py
@@ -12,6 +12,45 @@ import pickle
 
 # %% 1.1 Welcome to the course!
 
+filename = 'data/16/seaslug.txt'
+file = open(filename, mode='r')  # 'r' is to read, 'w' is to write
+text = file.read()
+file.close()
+
+print(text)
+
+with open(filename, 'r') as file:  # best practice
+    print(file.read())
+
+
+#%%
+
+# Open a file: file
+file = open('moby_dick.txt', 'r')
+
+# Print it
+print(file.read())
+
+# Check whether file is closed
+print(file.closed)
+
+# Close file
+file.close()
+
+# Check whether file is closed
+print(file.closed)
+
+#%%
+
+# Read & print the first 3 lines
+with open('moby_dick.txt') as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
+
+#%% 1.2 The importance of flat files in data science
+
+
 
 # %% 2. Importing data from other file types
 

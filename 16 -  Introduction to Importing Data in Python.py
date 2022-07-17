34f402740ca4f9a026445a089dbce5058cad5756
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-17 11:45:07 +0300

    fix

--- 16 -  Introduction to Importing Data in Python.py
+++ 16 -  Introduction to Importing Data in Python.py
@@ -8,24 +8,12 @@ from sas7bdat import SAS7BDAT
 from sqlalchemy import create_engine
 import pickle
 
+# %% 1. Introduction and flat files
 
-#%% 1. Introduction and flat files
-
-#%% 1.1 Welcome to the course!
-
+# %% 1.1 Welcome to the course!
 
 
+# %% 2. Importing data from other file types
 
 
-
-
-
-
-
-
-
-#%% 2. Importing data from other file types
-
-#%% 3. Working with relational databases in Python
-
-
+# %% 3. Working with relational databases in Python

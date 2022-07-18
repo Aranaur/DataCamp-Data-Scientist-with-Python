dd43fd1a18b4a3d7a5fe70f30b4307f7d8a8635c
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-18 16:16:15 +0300

    2.3

--- 16 -  Introduction to Importing Data in Python.py
+++ 16 -  Introduction to Importing Data in Python.py
@@ -48,11 +48,325 @@ with open('moby_dick.txt') as file:
     print(file.readline())
     print(file.readline())
 
-#%% 1.2 The importance of flat files in data science
+#%% 1.2 Importing flat files using NumPy
+
+filename = 'data/16/seaslug.txt'
+data = np.loadtxt(filename, delimiter='\t',
+                  skiprows=1,
+                  usecols=[0, 1],
+                  dtype=str)
+data
 
 
+#%%
+
+# Import package
+import numpy as np
+
+# Assign filename to variable: file
+file = 'data/16/digits.csv'
+
+# Load file as array: digits
+digits = np.loadtxt(file, delimiter=',')
+
+# Print datatype of digits
+print(type(digits))
+
+# Select and reshape a row
+im = digits[21, 1:]
+im_sq = np.reshape(im, (28, 28))
+
+# Plot reshaped data (matplotlib.pyplot already loaded as plt)
+plt.imshow(im_sq, cmap='Greys', interpolation='nearest')
+plt.show()
+
+#%%
+
+# Assign the filename: file
+file = 'digits_header.txt'
+
+# Load the data: data
+data = np.loadtxt(file, delimiter='\t', skiprows=1, usecols=[0, 2])
+
+# Print data
+print(data)
+
+#%%
+
+# Assign filename: file
+file = 'seaslug.txt'
+
+# Import file: data
+data = np.loadtxt(file, delimiter='\t', dtype=str)
+
+# Print the first element of data
+print(data[0])
+
+# Import data as floats and skip the first row: data_float
+data_float = np.loadtxt(file, delimiter='\t', dtype=float, skiprows=1)
+
+# Print the 10th element of data_float
+print(data_float[9])
+
+# Plot a scatterplot of the data
+plt.scatter(data_float[:, 0], data_float[:, 1])
+plt.xlabel('time (min.)')
+plt.ylabel('percentage of larvae')
+plt.show()
+
+#%%
+
+data = np.genfromtxt('data/16/titanic_sub.csv', delimiter=',', names=True, dtype=None, encoding='utf-8')
+data
+
+np.shape(data)
+data['Survived']
+
+#%%
+
+# Assign the filename: file
+file = 'data/16/titanic_sub.csv'
+
+# Import file using np.recfromcsv: d
+d = np.recfromcsv(file, delimiter=',', names=True, dtype=None, encoding='utf-8')
+
+# Print out first three entries of d
+print(d[:3])
+
+#%% 1.3 Importing flat files using pandas
+
+filename = 'data/16/titanic_sub.csv'
+data = pd.read_csv(filename)
+data.head()
+
+data_array = data.values
+
+#%%
+# Import pandas as pd
+import pandas as pd
+
+# Assign the filename: file
+file = 'titanic.csv'
+
+# Read the file into a DataFrame: df
+df = pd.read_csv(file)
+
+# View the head of the DataFrame
+print(df.head())
+
+#%%
+# Assign the filename: file
+file = 'digits.csv'
+
+# Read the first 5 rows of the file into a DataFrame: data
+data = pd.read_csv(file, nrows=5, header=None)
+
+# Build a numpy array from the DataFrame: data_array
+data_array = data.values
+
+# Print the datatype of data_array to the shell
+print(type(data_array))
+
+#%%
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Assign filename: file
+file = 'titanic_corrupt.txt'
+
+# Import file: data
+data = pd.read_csv(file, sep='\t', comment='#', na_values='Nothing')
+
+# Print the head of the DataFrame
+print(data.head())
+
+# Plot 'Age' variable in a histogram
+pd.DataFrame.hist(data[['Age']])
+plt.xlabel('Age (years)')
+plt.ylabel('count')
+plt.show()
 
 # %% 2. Importing data from other file types
 
+import pickle
+
+with open('pickled_fruit.pkl', 'rb') as file:
+    data = pickle.load(file)
+print(data)
+
+file = 'data/16/battledeath.xlsx'
+data = pd.ExcelFile(file)
+print(data.sheet_names)
+
+df1 = data.parse('2002')
+df2 = data.parse(1)
+
+#%%
+import os
+wd = os.getcwd()
+os.listdir(wd)
+
+#%%
+# Import pickle package
+import pickle
+
+# Open pickle file and load data: d
+with open('data.pkl', 'rb') as file:
+    d = pickle.load(file)
+
+# Print d
+print(d)
+
+# Print datatype of d
+print(type(d))
+
+#%%
+
+# Import pandas
+import pandas as pd
+
+# Assign spreadsheet filename: file
+file = 'battledeath.xlsx'
+
+# Load spreadsheet: xls
+xls = pd.ExcelFile(file)
+
+# Print sheet names
+print(xls.sheet_names)
+
+#%%
+
+# Load a sheet into a DataFrame by name: df1
+df1 = xls.parse('2004')
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Load a sheet into a DataFrame by index: df2
+df2 = xls.parse(0)
+
+# Print the head of the DataFrame df2
+print(df2.head())
+
+#%%
+
+# Parse the first sheet and rename the columns: df1
+df1 = xls.parse(0, skiprows=[0], names=['Country', 'AAM due to War (2002)'])
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Parse the first column of the second sheet and rename the column: df2
+df2 = xls.parse(1, usecols=[0], skiprows=[0], names=['Country'])
+
+# Print the head of the DataFrame df2
+print(df2.head())
+
+#%% 2.1 Importing SAS/Stata files using pandas
+
+from sas7bdat import SAS7BDAT
+with SAS7BDAT('data/16/sales.sas7bdat') as file:
+    df_sas = file.to_data_frame()
+df_sas
+
+data = pd.read_stata('data/16/disarea.dta')
+data
+
+#%%
+
+# Import sas7bdat package
+from sas7bdat import SAS7BDAT
+
+# Save file to a DataFrame: df_sas
+with SAS7BDAT('sales.sas7bdat') as file:
+    df_sas = file.to_data_frame()
+
+# Print head of DataFrame
+print(df_sas.head())
+
+# Plot histogram of DataFrame features (pandas and pyplot already imported)
+pd.DataFrame.hist(df_sas[['P']])
+plt.ylabel('count')
+plt.show()
+
+#%%
+
+# Import pandas
+import pandas as pd
+
+# Load Stata file into a pandas DataFrame: df
+df = pd.read_stata('disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of countries')
+plt.show()
+
+#%% 2.2 Importing HDF5 files
+
+import h5py
+filename = 'data/16/L-L1_LOSC_4_V1-1126259446-32.hdf5'
+data = h5py.File(filename, 'r')
+print(type(data))
+
+for key in data.keys():
+    print(key)
+
+print(type(data['meta']))
+
+for key in data['meta'].keys():
+    print(key)
+
+print(np.array(data['meta']['Description']), np.array(data['meta']['Detector']))
+
+#%%
+
+# Import packages
+import numpy as np
+import h5py
+
+# Assign filename: file
+file = 'LIGO_data.hdf5'
+
+# Load file: data
+data = h5py.File(file, 'r')
+
+# Print the datatype of the loaded file
+print(type(data))
+
+# Print the keys of the file
+for key in data.keys():
+    print(key)
+
+#%%
+# Get the HDF5 group: group
+group = data['strain']
+
+# Check out keys of group
+for key in group.keys():
+    print(key)
+
+# Set variable equal to time series data: strain
+strain = np.array(data['strain']['Strain'])
+
+# Set number of time points to sample: num_samples
+num_samples = 10000
+
+# Set time vector
+time = np.arange(0, 1, 1/num_samples)
+
+# Plot data
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
+
+
+#%% 2.3 Importing MATLAB files
 
 # %% 3. Working with relational databases in Python

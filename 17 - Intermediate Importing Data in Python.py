b416a779d06a083382456c1828b1dfe7cad89a53
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-19 18:56:26 +0300

    start 17

---
+++ 17 - Intermediate Importing Data in Python.py
@@ -0,0 +1,36 @@
+#%% 1. Importing data from the Internet
+
+#%% 1.1 Importing flat files from the web
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%% 2. Interacting with APIs to import data from the web
+
+#%% 3. Diving deep into the Twitter API

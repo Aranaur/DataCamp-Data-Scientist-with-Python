49471cde8368537b3046e8c9a35ce9ed0bd6dfd6
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-20 14:18:56 +0300

    fin 1

--- 17 - Intermediate Importing Data in Python.py
+++ 17 - Intermediate Importing Data in Python.py
@@ -1,36 +1,239 @@
 #%% 1. Importing data from the Internet
 
 #%% 1.1 Importing flat files from the web
+from urllib.request import urlretrieve
+import pandas as pd
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv'
+urlretrieve(url, 'data/17/winequality-white.csv')
 
+df = pd.read_csv('data/17/winequality-white.csv', sep=';')
+print(df.head())
 
+#%%
 
+# Import package
+from urllib.request import urlretrieve
 
+# Import pandas
+import pandas as pd
 
+# Assign url of file: url
+url = 'https://assets.datacamp.com/production/course_1606/datasets/winequality-red.csv'
 
+# Save file locally
+urlretrieve(url, 'winequality-red.csv')
 
+# Read file into a DataFrame and print its head
+df = pd.read_csv('winequality-red.csv', sep=';')
+print(df.head())
 
+#%%
 
+# Import packages
+import matplotlib.pyplot as plt
+import pandas as pd
 
+# Assign url of file: url
+url = 'https://assets.datacamp.com/production/course_1606/datasets/winequality-red.csv'
 
+# Read file into a DataFrame: df
+df = pd.read_csv(url, sep=';')
 
+# Print the head of the DataFrame
+print(df.head())
 
+# Plot first column of df
+df.iloc[:, 0].hist()
+plt.xlabel('fixed acidity (g(tartaric acid)/dm$^3$)')
+plt.ylabel('count')
+plt.show()
 
+#%%
+# Import package
+import pandas as pd
 
+# Assign url of file: url
+url = 'https://assets.datacamp.com/course/importing_data_into_r/latitude.xls'
 
+# Read in all sheets of Excel file: xls
+xls = pd.read_excel(url, sheet_name=None)
 
+# Print the sheetnames to the shell
+print(xls.keys())
 
+# Print the head of the first sheet (using its name, NOT its index)
+print(xls['1700'].head())
 
+#%% 1.2 HTTP requests to import files from the web
 
+from urllib.request import urlopen, Request
+url = 'https://www.wikipedia.org/'
+request = Request(url)
+response = urlopen(request)
+html = response.read()
+response.close()
 
+import requests
+url = 'https://www.wikipedia.org/'
+r = requests.get(url)
+text = r.text
 
+#%%
+# Import packages
+from urllib.request import urlopen, Request
 
+# Specify the url
+url = "https://campus.datacamp.com/courses/1606/4135?ex=2"
 
+# This packages the request: request
+request = Request(url)
 
+# Sends the request and catches the response: response
+response = urlopen(request)
 
+# Print the datatype of response
+print(type(response))
 
+# Be polite and close the response!
+response.close()
 
+#%%
+# Import packages
+from urllib.request import urlopen, Request
 
+# Specify the url
+url = "https://campus.datacamp.com/courses/1606/4135?ex=2"
+
+# This packages the request
+request = Request(url)
+
+# Sends the request and catches the response: response
+response = urlopen(request)
+
+# Extract the response: html
+html = response.read()
+
+# Print the html
+print(html)
+
+# Be polite and close the response!
+response.close()
+
+#%%
+# Import package
+import requests
+
+# Specify the url: url
+url = 'http://www.datacamp.com/teach/documentation'
+
+# Packages the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extract the response: text
+text = r.text
+
+# Print the html
+print(text)
+
+#%% 1.3 Scraping the web in Python
+
+from bs4 import BeautifulSoup
+import requests
+url = 'https://www.crummy.com/software/BeautifulSoup/'
+r = requests.get(url)
+html_doc = r.text
+soup = BeautifulSoup(html_doc)
+
+soup.prettify()
+soup.title
+soup.getText()
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+#%%
+# Import packages
+import requests
+from bs4 import BeautifulSoup
+
+# Specify url: url
+url = 'https://www.python.org/~guido/'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extracts the response as html: html_doc
+html_doc = r.text
+
+# Create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc)
+
+# Prettify the BeautifulSoup object: pretty_soup
+pretty_soup = soup.prettify()
+
+# Print the response
+print(pretty_soup)
+
+#%%
+# Import packages
+import requests
+from bs4 import BeautifulSoup
+
+# Specify url: url
+url = 'https://www.python.org/~guido/'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extract the response as html: html_doc
+html_doc = r.text
+
+# Create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc)
+
+# Get the title of Guido's webpage: guido_title
+guido_title = soup.title
+
+# Print the title of Guido's webpage to the shell
+print(guido_title)
+
+# Get Guido's text: guido_text
+guido_text = soup.getText()
+
+# Print Guido's text to the shell
+print(guido_text)
+
+#%%
+
+# Import packages
+import requests
+from bs4 import BeautifulSoup
+
+# Specify url
+url = 'https://www.python.org/~guido/'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extracts the response as html: html_doc
+html_doc = r.text
+
+# create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc)
+
+# Print the title of Guido's webpage
+print(soup.title)
+
+# Find all 'a' tags (which define hyperlinks): a_tags
+a_tags = 'a'
+
+# Print the URLs to the shell
+for link in soup.find_all(a_tags):
+    print(link.get('href'))
 
 #%% 2. Interacting with APIs to import data from the web
+
+#%% 2.1 Introduction to APIs and JSONs
+
 
 #%% 3. Diving deep into the Twitter API

bff1f47646bd7c414ba2baa66f7998ca1598b14d
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-23 00:16:07 +0300

    fin all

--- 17 - Intermediate Importing Data in Python.py
+++ 17 - Intermediate Importing Data in Python.py
@@ -311,3 +311,94 @@ print(pizza_extract)
 #%% 3. Diving deep into the Twitter API
 
 #%% 3.1 The Twitter API and Authentication
+
+import tweepy, json
+
+access_token = ''
+access_token_secret = ''
+consumer_key = ''
+consumer_secret = ''
+
+stream = tweepy.Stream(consumer_key, consumer_secret,
+                       access_token, access_token_secret)
+
+stream.filter(track=['apples', 'oranges'])
+
+#%%
+
+# Store credentials in relevant variables
+consumer_key = "nZ6EA0FxZ293SxGNg8g8aP0HM"
+consumer_secret = "fJGEodwe3KiKUnsYJC3VRndj7jevVvXbK2D5EiJ2nehafRgA6i"
+access_token = "1092294848-aHN7DcRP9B4VMTQIhwqOYiB14YkW92fFO8k8EPy"
+access_token_secret = "X4dHmhPfaksHcQ7SCbmZa2oYBBVSD2g8uIHXsp5CTaksx"
+
+# Create your Stream object with credentials
+stream = tweepy.Stream(consumer_key, consumer_secret,
+                       access_token, access_token_secret)
+
+# Filter your Stream variable
+stream.filter(["clinton", "trump", "sanders", "cruz"])
+
+#%%
+
+# Import package
+import json
+
+# String of path to file: tweets_data_path
+tweets_data_path = 'tweets.txt'
+
+# Initialize empty list to store tweets: tweets_data
+tweets_data = []
+
+# Open connection to file
+tweets_file = open(tweets_data_path, "r")
+
+# Read in tweets and store in list: tweets_data
+for line in tweets_file:
+    tweet = json.loads(line)
+    tweets_data.append(tweet)
+
+# Close connection to file
+tweets_file.close()
+
+# Print the keys of the first tweet dict
+print(tweets_data[0].keys())
+
+#%%
+# Import package
+import pandas as pd
+
+# Build DataFrame of tweet texts and languages
+df = pd.DataFrame(tweets_data, columns=['text', 'lang'])
+
+# Print head of DataFrame
+print(df.head())
+
+#%%
+# Initialize list to store tweet counts
+[clinton, trump, sanders, cruz] = [0, 0, 0, 0]
+
+# Iterate through df, counting the number of tweets in which
+# each candidate is mentioned
+for index, row in df.iterrows():
+    clinton += word_in_text('clinton', row['text'])
+    trump += word_in_text('trump', row['text'])
+    sanders += word_in_text('sanders', row['text'])
+    cruz += word_in_text('cruz', row['text'])
+
+#%%
+# Import packages
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['clinton', 'trump', 'sanders', 'cruz']
+
+# Plot the bar chart
+ax = sns.barplot(cd, [clinton, trump, sanders, cruz])
+ax.set(ylabel="count")
+plt.show()

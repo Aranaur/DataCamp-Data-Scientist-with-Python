ecc06971f95e3c0fd6cf271d07516246a5e8bcd5
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-22 12:03:58 +0300

    fin 2

--- 17 - Intermediate Importing Data in Python.py
+++ 17 - Intermediate Importing Data in Python.py
@@ -234,6 +234,80 @@ for link in soup.find_all(a_tags):
 #%% 2. Interacting with APIs to import data from the web
 
 #%% 2.1 Introduction to APIs and JSONs
+import json
+with open ('data/17/', 'r') as json_file:
+    json_data = json.load(json_file)
 
+type(json_data)
+
+for key, value in json_data.items():
+    print(key + ':', value)
+
+#%%
+# Load JSON: json_data
+with open("a_movie.json") as json_file:
+    json_data = json.load(json_file)
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
+
+#%% 2.2 APIs and interacting with the world wide web
+
+import requests
+url = 'http://www.omdbapi.com/?t=hackers'
+r = requests.get(url)
+json_data = r.json()
+for key, value in json_data.items():
+    print(key + ':', value)
+
+#%%
+# Import requests package
+import requests
+
+# Assign URL to variable: url
+url = 'http://www.omdbapi.com/?apikey=72bc447a&t=the+social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Print the text of the response
+print(r.text)
+
+#%%
+# Import package
+import requests
+
+# Assign URL to variable: url
+url = 'http://www.omdbapi.com/?apikey=72bc447a&t=social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
+
+#%%
+# Import package
+import requests
+
+# Assign URL to variable: url
+url = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=pizza'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print the Wikipedia page extract
+pizza_extract = json_data['query']['pages']['24768']['extract']
+print(pizza_extract)
 
 #%% 3. Diving deep into the Twitter API
+
+#%% 3.1 The Twitter API and Authentication

2900527fd0ac997a795156a2911ed3182ac0cea6
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-07-28 21:59:17 +0300

    fin 2.2

--- 19 - Working with Dates and Times in Python.py
+++ 19 - Working with Dates and Times in Python.py
@@ -13,9 +13,9 @@ florida_hurricane_dates = sorted(florida_hurricane_dates)
 
 rides.head()  # Display the first five rows
 
-#%% 1. Dates and Calendars
+# %% 1. Dates and Calendars
 
-#%% 1.1 Dates in Python
+# %% 1.1 Dates in Python
 
 two_hurricanes = ['10/7/2016', '6/21/2017']
 
@@ -25,7 +25,7 @@ two_hurricanes_dates[0].month
 two_hurricanes_dates[0].day
 two_hurricanes_dates[0].weekday()
 
-#%%
+# %%
 # Import date from datetime
 from datetime import date
 
@@ -35,7 +35,7 @@ hurricane_andrew = date(1992, 8, 24)
 # Which day of the week is the date?
 print(hurricane_andrew.weekday())
 
-#%%
+# %%
 
 # Counter for how many before June 1
 early_hurricanes = 0
@@ -48,7 +48,7 @@ for hurricane in florida_hurricane_dates:
 
 print(early_hurricanes)
 
-#%% 1.2 Math with dates
+# %% 1.2 Math with dates
 a = 11
 b = 14
 l = [a, b]
@@ -63,10 +63,11 @@ delta = d2 - d1
 print(delta.days)
 
 from datetime import timedelta
+
 td = timedelta(days=29)
 print(d1 + td)
 
-#%%
+# %%
 # Import date
 from datetime import date
 
@@ -79,10 +80,10 @@ end = date(2007, 12, 13)
 # Subtract the two dates and print the number of days
 print((end - start).days)
 
-#%%
+# %%
 # A dictionary to count hurricanes per calendar month
-hurricanes_each_month = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6:0,
-                         7: 0, 8:0, 9:0, 10:0, 11:0, 12:0}
+hurricanes_each_month = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0,
+                         7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0}
 
 # Loop over all hurricanes
 for hurricane in florida_hurricane_dates:
@@ -93,7 +94,7 @@ for hurricane in florida_hurricane_dates:
 
 print(hurricanes_each_month)
 
-#%%
+# %%
 # Print the first and last scrambled dates
 print(dates_scrambled[0])
 print(dates_scrambled[-1])
@@ -105,7 +106,7 @@ dates_ordered = sorted(dates_scrambled)
 print(dates_ordered[0])
 print(dates_ordered[-1])
 
-#%% 1.3 Turning dates into strings
+# %% 1.3 Turning dates into strings
 
 d = date(2017, 11, 5)
 print(d)
@@ -119,7 +120,7 @@ d.strftime('%Y')
 d.strftime('Year is %Y')
 d.strftime('%Y/%m/%d')
 
-#%%
+# %%
 # Assign the earliest date to first_date
 first_date = min(florida_hurricane_dates)
 
@@ -130,7 +131,7 @@ us = "Our earliest hurricane date: " + first_date.strftime("%m/%d/%Y")
 print("ISO: " + iso)
 print("US: " + us)
 
-#%%
+# %%
 # Import date
 from datetime import date
 
@@ -140,7 +141,7 @@ andrew = date(1992, 8, 26)
 # Print the date in the format 'YYYY-MM'
 print(andrew.strftime('%Y-%m'))
 
-#%%
+# %%
 # Import date
 from datetime import date
 
@@ -150,7 +151,7 @@ andrew = date(1992, 8, 26)
 # Print the date in the format 'MONTH (YYYY)'
 print(andrew.strftime('%B (%Y)'))
 
-#%%
+# %%
 # Import date
 from datetime import date
 
@@ -160,6 +161,175 @@ andrew = date(1992, 8, 26)
 # Print the date in the format 'YYYY-DDD'
 print(andrew.strftime('%Y-%j'))
 
-#%% 2. Combining Dates and Times
+# %% 2. Combining Dates and Times
 
-#%% 2.1 Dates and times
+# %% 2.1 Dates and times
+
+from datetime import datetime
+
+dt = datetime(2017, 10, 1, 15, 23, 25, 500000)
+
+dt_hr = dt.replace(minute=0, second=0, microsecond=0)
+print(dt_hr)
+
+# %%
+# Import datetime
+from datetime import datetime
+
+# Create a datetime object
+dt = datetime(2017, 10, 1, 15, 26, 26)
+
+# Print the results in ISO 8601 format
+print(dt.isoformat())
+
+# %%
+# Import datetime
+from datetime import datetime
+
+# Create a datetime object
+dt = datetime(2017, 12, 31, 15, 19, 13)
+
+# Print the results in ISO 8601 format
+print(dt.isoformat())
+
+# %%
+# Import datetime
+from datetime import datetime
+
+# Create a datetime object
+dt = datetime(2017, 12, 31, 15, 19, 13)
+
+# Replace the year with 1917
+dt_old = dt.replace(year=1917)
+
+# Print the results in ISO 8601 format
+print(dt_old)
+
+# %%
+
+# Create dictionary to hold results
+trip_counts = {'AM': 0, 'PM': 0}
+
+# Loop over all trips
+for trip in onebike_datetimes:
+    # Check to see if the trip starts before noon
+    if trip['start'].hour < 12:
+        # Increment the counter for before noon
+        trip_counts['AM'] += 1
+    else:
+        # Increment the counter for after noon
+        trip_counts['PM'] += 1
+
+print(trip_counts)
+
+# %% 2.2 Printing and parsing datetimes
+
+dt = datetime(2017, 12, 30, 15, 19, 13)
+
+print(dt.strftime('%Y-%m-%d'))
+print(dt.strftime('%Y-%m-%d %H:%M:%S'))
+print(dt.strftime('%H:%M:%S on %Y/%m/%d'))
+print(dt.isoformat())
+
+dt = datetime.strptime('12/30/2017 15:19:13', '%m/%d/%Y %H:%M:%S')
+print(type(dt))
+print(dt)
+
+ts = 1514665153.0  # sec since 1 January 1970
+print(datetime.fromtimestamp(ts))
+
+#%%
+# Import the datetime class
+from datetime import datetime
+
+# Starting string, in YYYY-MM-DD HH:MM:SS format
+s = '2017-02-03 00:00:01'
+
+# Write a format string to parse s
+fmt = '%Y-%m-%d %H:%M:%S'
+
+# Create a datetime object d
+d = datetime.strptime(s, fmt)
+
+# Print d
+print(d)
+
+#%%
+# Import the datetime class
+from datetime import datetime
+
+# Starting string, in YYYY-MM-DD format
+s = '2030-10-15'
+
+# Write a format string to parse s
+fmt = '%Y-%m-%d'
+
+# Create a datetime object d
+d = datetime.strptime(s, fmt)
+
+# Print d
+print(d)
+
+#%%
+# Import the datetime class
+from datetime import datetime
+
+# Starting string, in MM/DD/YYYY HH:MM:SS format
+s = '12/15/1986 08:00:00'
+
+# Write a format string to parse s
+fmt = '%m/%d/%Y %H:%M:%S'
+
+# Create a datetime object d
+d = datetime.strptime(s, fmt)
+
+# Print d
+print(d)
+
+#%%
+# Write down the format string
+fmt = "%Y-%m-%d %H:%M:%S"
+
+# Initialize a list for holding the pairs of datetime objects
+onebike_datetimes = []
+
+# Loop over all trips
+for (start, end) in onebike_datetime_strings:
+    trip = {'start': datetime.strptime(start, fmt),
+            'end': datetime.strptime(end, fmt)}
+
+    # Append the trip
+    onebike_datetimes.append(trip)
+
+#%%
+# Import datetime
+from datetime import datetime
+
+# Pull out the start of the first trip
+first_start = onebike_datetimes[0]['start']
+
+# Format to feed to strftime()
+fmt = "%Y-%m-%dT%H:%M:%S"
+
+# Print out date with .isoformat(), then with .strftime() to compare
+print(first_start.isoformat())
+print(first_start.strftime(fmt))
+
+#%%
+# Import datetime
+from datetime import datetime
+
+# Starting timestamps
+timestamps = [1514665153, 1514664543]
+
+# Datetime objects
+dts = []
+
+# Loop
+for ts in timestamps:
+    dts.append(datetime.fromtimestamp(ts))
+
+# Print results
+print(dts)
+
+#%% 2.3 Working with durations

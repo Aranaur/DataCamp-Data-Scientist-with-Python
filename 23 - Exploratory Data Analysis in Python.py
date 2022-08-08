6a4082c11bad96c393828f131713eed9b44222ca
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-08-08 20:49:21 +0300

    fin 1

--- 23 - Exploratory Data Analysis in Python.py
+++ 23 - Exploratory Data Analysis in Python.py
@@ -101,3 +101,50 @@ preterm_weight.mean()
 full_term_weight = birth_weight[~preterm]
 full_term_weight.sum()
 full_term_weight.mean()
+
+#%%
+# Plot the histogram
+plt.hist(agecon, bins=20)
+
+# Label the axes
+plt.xlabel('Age at conception')
+plt.ylabel('Number of pregnancies')
+
+# Show the figure
+plt.show()
+
+#%%
+# Plot the histogram
+plt.hist(agecon, bins=20, histtype='step')
+
+# Label the axes
+plt.xlabel('Age at conception')
+plt.ylabel('Number of pregnancies')
+
+# Show the figure
+plt.show()
+
+#%%
+# Create a Boolean Series for full-term babies
+full_term = nsfg['prglngth'] >= 37
+
+# Select the weights of full-term babies
+full_term_weight = birth_weight[full_term]
+
+# Compute the mean weight of full-term babies
+print(full_term_weight.mean())
+
+#%%
+# Filter full-term babies
+full_term = nsfg['prglngth'] >= 37
+
+# Filter single births
+single = nsfg['nbrnaliv'] == 1
+
+# Compute birth weight for single full-term babies
+single_full_term_weight = birth_weight[full_term & single]
+print('Single full-term mean:', single_full_term_weight.mean())
+
+# Compute birth weight for multiple full-term babies
+mult_full_term_weight = birth_weight[full_term & ~single]
+print('Multiple full-term mean:', mult_full_term_weight.mean())

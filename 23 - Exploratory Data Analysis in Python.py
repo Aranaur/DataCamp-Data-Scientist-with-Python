1d840dcd6b74a6a31797e56b29ef68b4db3324f4
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-08-10 13:58:06 +0300

    fin 3

--- 23 - Exploratory Data Analysis in Python.py
+++ 23 - Exploratory Data Analysis in Python.py
@@ -378,3 +378,210 @@ plt.show()
 #%% 3. Relationships
 
 #%% 3.1 Exploring relationships
+height = brfss['HTM4']
+weight = brfss['WTKG3']
+
+plt.scatter(x=height, y=weight)
+plt.show()
+
+plt.plot(height, weight, 'o', alpha=0.02, markersize=1)
+plt.xlabel('Height in cm')
+plt.ylabel('Weight in kg')
+plt.show()
+
+height_jitter = height + np.random.normal(0, 2, size=len(brfss))
+weight_jitter = weight + np.random.normal(0, 2, size=len(brfss))
+
+plt.plot(height_jitter, weight_jitter, 'o', alpha=0.02, markersize=1)
+plt.axis([140, 200, 0, 160])
+plt.xlabel('Height in cm')
+plt.ylabel('Weight in kg')
+plt.show()
+
+# sns.scatterplot('HTM4', 'WTKG3', data=brfss, alpha=0.02, x_jitter=0.9, y_jitter=0.9, size=0.01)
+
+#%%
+# Extract age
+age = brfss['AGE']
+
+# Plot the PMF
+pmf_age = Pmf(age)
+pmf_age.bar()
+
+# Label the axes
+plt.xlabel('Age in years')
+plt.ylabel('PMF')
+plt.show()
+
+#%%
+# Select the first 1000 respondents
+brfss = brfss[:1000]
+
+# Extract age and weight
+age = brfss['AGE']
+weight = brfss['WTKG3']
+
+# Make a scatter plot
+plt.plot(age, weight, 'o', alpha=0.1)
+
+plt.xlabel('Age in years')
+plt.ylabel('Weight in kg')
+
+plt.show()
+
+#%%
+# Select the first 1000 respondents
+brfss = brfss[:1000]
+
+# Add jittering to age
+age = brfss['AGE'] + np.random.normal(0, 2.5, size=len(brfss))
+# Extract weight
+weight = brfss['WTKG3']
+
+# Make a scatter plot
+plt.plot(age, weight, 'o', markersize=5, alpha=0.2)
+
+plt.xlabel('Age in years')
+plt.ylabel('Weight in kg')
+plt.show()
+
+#%% 3.2 Visualizing relationships
+age = brfss['AGE'] + np.random.normal(0, 0.5, size=len(brfss))
+weight = brfss['WTKG3'] + np.random.normal(0, 2, size=len(brfss))
+plt.plot(age, weight, 'o', markersize=1, alpha=0.2)
+plt.axis([15, 95, 0, 150])
+plt.xlabel('Age in years')
+plt.ylabel('Weight in kg')
+plt.show()
+
+data = brfss.dropna(subset=['AGE', 'WTKG3'])
+
+sns.violinplot('AGE', 'WTKG3', data=data, inner=None)
+plt.show()
+
+sns.boxplot('AGE', 'WTKG3', data=data, whis=10)
+plt.yscale('log')
+plt.show()
+
+#%%
+# Drop rows with missing data
+data = brfss.dropna(subset=['_HTMG10', 'WTKG3'])
+
+# Make a box plot
+sns.boxplot(x='_HTMG10', y='WTKG3', data=data, whis=10)
+
+# Plot the y-axis on a log scale
+plt.yscale('log')
+
+# Remove unneeded lines and label axes
+sns.despine(left=True, bottom=True)
+plt.xlabel('Height in cm')
+plt.ylabel('Weight in kg')
+plt.show()
+
+#%%
+# Extract income
+income = brfss['INCOME2']
+
+# Plot the PMF
+Pmf(income).bar()
+
+# Label the axes
+plt.xlabel('Income level')
+plt.ylabel('PMF')
+plt.show()
+
+#%%
+# Drop rows with missing data
+data = brfss.dropna(subset=['INCOME2', 'HTM4'])
+
+# Make a violin plot
+sns.violinplot(x='INCOME2', y='HTM4', data=data, inner=None)
+
+# Remove unneeded lines and label axes
+sns.despine(left=True, bottom=True)
+plt.xlabel('Income level')
+plt.ylabel('Height in cm')
+plt.show()
+
+#%% 2.3 Correlation
+columns = ['HTM4', 'WTKG3', 'AGE']
+subset = brfss[columns]
+
+subset.corr()
+
+xs = np.linspace(-1, 1)
+ys = xs**2
+ys += np.random.normal(0, 0.05, len(xs))
+
+plt.plot(xs, ys, 'o')
+plt.show()
+
+np.corrcoef(xs, ys)
+
+#%%
+# Select columns
+columns = ['AGE', 'INCOME2', '_VEGESU1']
+subset = brfss[columns]
+
+# Compute the correlation matrix
+print(subset.corr())
+
+#%% 3.3 Simple regression
+from scipy.stats import linregress
+
+res = linregress(xs, ys)
+print(res)
+
+fx = np.array([xs.min(), xs.max()])
+fy = res.intercept + res.slope * fx
+plt.plot(xs, ys, 'o')
+plt.plot(fx, fy, '-')
+
+#%%
+subset = brfss.dropna(subset=['WTKG3', 'HTM4'])
+
+xs = subset['HTM4']
+ys = subset['WTKG3']
+res = linregress(xs, ys)
+
+fx = np.array([xs.min(), xs.max()])
+fy = res.intercept + res.slope * fx
+plt.plot(fx, fy, '-')
+plt.plot(height_jitter, weight_jitter, 'o', alpha=0.02, markersize=1)
+plt.axis([140, 200, 0, 160])
+plt.xlabel('Height in cm')
+plt.ylabel('Weight in kg')
+plt.show()
+
+#%%
+from scipy.stats import linregress
+
+# Extract the variables
+subset = brfss.dropna(subset=['INCOME2', '_VEGESU1'])
+xs = subset['INCOME2']
+ys = subset['_VEGESU1']
+
+# Compute the linear regression
+res = linregress(xs, ys)
+print(res)
+
+#%%
+# Plot the scatter plot
+plt.clf()
+x_jitter = xs + np.random.normal(0, 0.15, len(xs))
+plt.plot(x_jitter, ys, 'o', alpha=0.2)
+
+# Plot the line of best fit
+fx = np.array([xs.min(), xs.max()])
+fy = res.intercept + res.slope * fx
+plt.plot(fx, fy, '-', alpha=0.7)
+
+plt.xlabel('Income code')
+plt.ylabel('Vegetable servings per day')
+plt.ylim([0, 6])
+plt.show()
+
+#%% 4. Multivariate Thinking
+
+#%% 4.1 Limits of simple regression

326a13be0c45c08b3018070b8f98d6f7be7dccc3
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-08-17 14:07:24 +0300

    start 1

---
+++ 26 - Introduction to Regression with statsmodels in Python.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+churn = pd.read_csv('data/26/churn.csv')
+fish = pd.read_csv('data/26/fish.csv')
+ad_conversion = pd.read_csv('data/26/ad_conversion.csv')
+sp500_yearly_returns = pd.read_csv('data/26/sp500_yearly_returns.csv')
+taiwan_real_estate2 = pd.read_csv('data/26/taiwan_real_estate2.csv')
+
+#%% 1. Simple Linear Regression Modeling

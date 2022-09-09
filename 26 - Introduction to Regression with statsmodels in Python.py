86fd2892b6525568b2abcd94ac234de7712b7a87
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-09-09 22:16:35 +0300

    fin 2

--- 26 - Introduction to Regression with statsmodels in Python.py
+++ 26 - Introduction to Regression with statsmodels in Python.py
@@ -385,3 +385,84 @@ sns.regplot(x='sqer_spend',
             y='sqer_n_impressions',
             data=ad_conversion,
             ci=None)
+
+#%%
+mdl_ad = ols('sqer_n_impressions ~ sqer_spend', data=ad_conversion).fit()
+
+explanatory_data = pd.DataFrame({
+    'sqer_spend': np.sqrt(np.arange(0, 601, 100)),
+    'spent_usd': np.arange(0, 601, 100)
+})
+
+prediction_data = explanatory_data.assign(sqrt_n_impress=mdl_ad.predict(explanatory_data),
+                                          n_impression=mdl_ad.predict(explanatory_data) ** 2)
+
+prediction_data
+
+#%%
+# Create sqrt_dist_to_mrt_m
+taiwan_real_estate["sqrt_dist_to_mrt_m"] = np.sqrt(taiwan_real_estate["dist_to_mrt_m"])
+
+plt.figure()
+
+# Plot using the transformed variable
+sns.regplot(x='sqrt_dist_to_mrt_m',
+            y='price_twd_msq',
+            data=taiwan_real_estate,
+            ci=None)
+plt.show()
+
+#%%
+# Create sqrt_dist_to_mrt_m
+taiwan_real_estate["sqrt_dist_to_mrt_m"] = np.sqrt(taiwan_real_estate["dist_to_mrt_m"])
+
+# Run a linear regression of price_twd_msq vs. sqrt_dist_to_mrt_m
+mdl_price_vs_dist = ols("price_twd_msq ~ sqrt_dist_to_mrt_m", data=taiwan_real_estate).fit()
+
+# Use this explanatory data
+explanatory_data = pd.DataFrame({"sqrt_dist_to_mrt_m": np.sqrt(np.arange(0, 81, 10) ** 2),
+                                 "dist_to_mrt_m": np.arange(0, 81, 10) ** 2})
+
+# Use mdl_price_vs_dist to predict explanatory_data
+prediction_data = explanatory_data.assign(
+    price_twd_msq = mdl_price_vs_dist.predict(explanatory_data)
+)
+
+fig = plt.figure()
+sns.regplot(x="sqrt_dist_to_mrt_m", y="price_twd_msq", data=taiwan_real_estate, ci=None)
+
+# Add a layer of your prediction points
+sns.scatterplot(data=prediction_data, x='sqrt_dist_to_mrt_m', y='price_twd_msq', color='r')
+plt.show()
+
+#%%
+ad_conversion["qdrt_n_impressions"] = ad_conversion["n_impressions"] ** 0.25
+ad_conversion["qdrt_n_clicks"] = ad_conversion["n_clicks"] ** 0.25
+
+mdl_click_vs_impression = ols("qdrt_n_clicks ~ qdrt_n_impressions", data=ad_conversion, ci=None).fit()
+
+explanatory_data = pd.DataFrame({"qdrt_n_impressions": np.arange(0, 3e6+1, 5e5) ** .25,
+                                 "n_impressions": np.arange(0, 3e6+1, 5e5)})
+
+# Complete prediction_data
+prediction_data = explanatory_data.assign(
+    qdrt_n_clicks = mdl_click_vs_impression.predict(explanatory_data)
+)
+
+# Print the result
+print(prediction_data)
+#%%
+# Back transform qdrt_n_clicks
+prediction_data["n_clicks"] = prediction_data["qdrt_n_clicks"] ** 4
+
+# Plot the transformed variables
+fig = plt.figure()
+sns.regplot(x="qdrt_n_impressions", y="qdrt_n_clicks", data=ad_conversion, ci=None)
+
+# Add a layer of your prediction points
+sns.scatterplot(data=prediction_data, x='qdrt_n_impressions', y='qdrt_n_clicks', color='r')
+plt.show()
+
+#%% 3. Assessing model fit
+
+#%% 3.1 Quantifying model fit

c37ce6c94e727d597e5f061e538076c9b1ca36f8
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2022-09-10 13:05:01 +0300

    fin 3.2

--- 26 - Introduction to Regression with statsmodels in Python.py
+++ 26 - Introduction to Regression with statsmodels in Python.py
@@ -466,3 +466,95 @@ plt.show()
 #%% 3. Assessing model fit
 
 #%% 3.1 Quantifying model fit
+
+print(bream_ols.summary())
+
+bream_ols.rsquared
+
+bream['length_cm'].corr(bream['mass_g']) ** 2
+
+bream_ols.mse_resid
+
+np.sqrt(bream_ols.mse_resid)
+
+# RSE
+df = len(bream.index) - 2
+np.sqrt(sum(bream_ols.resid ** 2) / df)
+
+# RMSE
+np.sqrt(sum(bream_ols.resid ** 2) / len(bream.index))
+
+#%%
+# Calculate mse_orig for mdl_click_vs_impression_orig
+mse_orig = mdl_click_vs_impression_orig.mse_resid
+
+# Calculate rse_orig for mdl_click_vs_impression_orig and print it
+rse_orig = np.sqrt(mse_orig)
+print("RSE of original model: ", rse_orig)
+
+# Calculate mse_trans for mdl_click_vs_impression_trans
+mse_trans = mdl_click_vs_impression_trans.mse_resid
+
+# Calculate rse_trans for mdl_click_vs_impression_trans and print it
+rse_trans = np.sqrt(mse_trans)
+print("RSE of transformed model: ", rse_trans)
+
+#%% 3.2 Visualizing model fit
+# Resid vs fitted
+sns.residplot(x='length_cm',
+              y='mass_g',
+              data=bream,
+              lowess=True)
+plt.xlabel('Fitted values')
+plt.ylabel('Residuals')
+plt.show()
+
+# QQ plot
+from statsmodels.api import qqplot
+qqplot(data=bream_ols.resid, fit=True, line='45')
+plt.show()
+
+# Scale-location plot
+model_norm_resud_bream = bream_ols.get_influence().resid_studentized_internal
+model_norm_abs_sqrt_bream = np.sqrt(np.abs(model_norm_resud_bream))
+sns.regplot(x=bream_ols.fittedvalues,
+            y=model_norm_abs_sqrt_bream,
+            ci=None,
+            lowess=True)
+plt.xlabel('Fitted values')
+plt.ylabel('Sqrt of abs val of std resid')
+plt.show()
+
+#%%
+# Plot the residuals vs. fitted values
+sns.residplot(x='n_convenience', y='price_twd_msq', data=taiwan_real_estate, lowess=True)
+plt.xlabel("Fitted values")
+plt.ylabel("Residuals")
+
+# Show the plot
+plt.show()
+
+#%%
+# Import qqplot
+from statsmodels.api import qqplot
+
+# Create the Q-Q plot of the residuals
+qqplot(data=mdl_price_vs_conv.resid, fit=True, line="45")
+
+# Show the plot
+plt.show()
+
+#%%
+# Preprocessing steps
+model_norm_residuals = mdl_price_vs_conv.get_influence().resid_studentized_internal
+model_norm_residuals_abs_sqrt = np.sqrt(np.abs(model_norm_residuals))
+
+# Create the scale-location plot
+sns.regplot(x=mdl_price_vs_conv.fittedvalues, y=model_norm_residuals_abs_sqrt, ci=None, lowess=True)
+plt.xlabel("Fitted values")
+plt.ylabel("Sqrt of abs val of stdized residuals")
+
+# Show the plot
+plt.show()
+
+#%% 3.3 Outliers, leverage, and influence

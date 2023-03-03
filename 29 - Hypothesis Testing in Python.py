4a5efb0b32006430b88b430997c4677e1f7bd215
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2023-03-04 00:49:32 +0200

    29.3 fin

--- 29 - Hypothesis Testing in Python.py
+++ 29 - Hypothesis Testing in Python.py
@@ -219,3 +219,125 @@ pairwise_results = pingouin.pairwise_tests(data=late_shipments,
 print(pairwise_results)
 
 #%% 3. Proportion Tests
+# %%  3.1 One-sample proportion tests
+from scipy.stats import norm
+
+# Left-tailed: less then
+p_value = norm.cdf(z_score)
+
+# Right-tailed: greater then
+p_value = 1 - norm.cdf(z_score)
+
+# Two-tailed: not equal
+p_value = norm.cdf(-z_score) + 1 - norm.cdf(z_score)
+p_value = 2 * (1 - norm.cdf(z_score))
+
+# %% Test for single proportions
+
+# Hypothesize that the proportion of late shipments is 6%
+p_0 = 0.06
+
+# Calculate the sample proportion of late shipments
+p_hat = (late_shipments['late'] == "Yes").mean()
+
+# Calculate the sample size
+n = len(late_shipments)
+
+# Calculate the numerator and denominator of the test statistic
+numerator = p_hat - p_0
+denominator = np.sqrt(p_0 * (1 - p_0) / n)
+
+# Calculate the test statistic
+z_score = numerator / denominator
+
+# Calculate the p-value from the z-score
+p_value = 1 - norm.cdf(z_score)
+
+# Print the p-value
+print(p_value)
+
+# %% Two-sample proportion tests
+# Test of two proportions
+
+# Calculate the pooled estimate of the population proportion
+p_hat = (p_hats["reasonable"] * ns["reasonable"] + p_hats["expensive"] * ns["expensive"]) / (ns["reasonable"] + ns["expensive"])
+
+# Calculate p_hat one minus p_hat
+p_hat_times_not_p_hat = p_hat * (1 - p_hat)
+
+# Divide this by each of the sample sizes and then sum
+p_hat_times_not_p_hat_over_ns = p_hat_times_not_p_hat / ns["expensive"] + p_hat_times_not_p_hat / ns["reasonable"]
+
+# Calculate the standard error
+std_error = np.sqrt(p_hat_times_not_p_hat_over_ns)
+
+# Calculate the z-score
+z_score = (p_hats["expensive"] - p_hats["reasonable"]) / std_error
+
+# Calculate the p-value from the z-score
+p_value = 1 - norm.cdf(z_score)
+
+# Print p_value
+print(p_value)
+
+# %% proportions_ztest() for two samples
+
+# Count the late column values for each freight_cost_group
+late_by_freight_cost_group = late_shipments.groupby("freight_cost_group")['late'].value_counts()
+
+# Create an array of the "Yes" counts for each freight_cost_group
+success_counts = np.array([45, 16])
+
+# Create an array of the total number of rows in each freight_cost_group
+n = np.array([45 + 500, 16 + 439])
+
+# Run a z-test on the two proportions
+stat, p_value = proportions_ztest(count=success_counts, nobs=n,
+                                  alternative="larger")
+
+# Print the results
+print(stat, p_value)
+
+# %% Chi-square test of independence
+
+# Proportion of freight_cost_group grouped by vendor_inco_term
+props = late_shipments.groupby('vendor_inco_term')['freight_cost_group'].value_counts(normalize=True)
+
+# Convert props to wide format
+wide_props = props.unstack()
+
+# Proportional stacked bar plot of freight_cost_group vs. vendor_inco_term
+wide_props.plot(kind="bar", stacked=True)
+plt.show()
+
+# Determine if freight_cost_group and vendor_inco_term are independent
+expected, observed, stats = pingouin.chi2_independence(data=late_shipments, x="vendor_inco_term", y="freight_cost_group")
+
+# Print results
+print(stats[stats['test'] == 'pearson']) 
+
+# %% Chi-square goodness of fit tests
+
+# Find the number of rows in late_shipments
+n_total = len(late_shipments)
+
+# Create n column that is prop column * n_total
+hypothesized["n"] = hypothesized["prop"] * n_total
+
+# Plot a red bar graph of n vs. vendor_inco_term for incoterm_counts
+plt.bar(incoterm_counts['vendor_inco_term'], incoterm_counts['n'], color="red", label="Observed")
+
+# Add a blue bar plot for the hypothesized counts
+plt.bar(hypothesized['vendor_inco_term'], hypothesized['n'], alpha=0.5, color="blue", label="Hypothesized")
+plt.legend()
+plt.show()
+
+# Performing a goodness of fit test
+# Perform a goodness of fit test on the incoterm counts n
+gof_test = chisquare(f_obs=incoterm_counts['n'], 
+                     f_exp=hypothesized['n'])
+
+# Print gof_test results
+print(gof_test)
+
+# %% 4. Non-Parametric Tests

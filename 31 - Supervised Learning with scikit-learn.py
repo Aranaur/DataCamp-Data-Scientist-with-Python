7e999b8d11d91949efb0d63dddaa91611515a3fb
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2023-03-04 01:42:40 +0200

    fin 30

---
+++ 31 - Supervised Learning with scikit-learn.py
@@ -0,0 +1,39 @@
+# %% 1. Classification
+# Machine learning with scikit-learn
+# %% 
+# Import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier 
+
+# Create arrays for the features and the target variable
+y = churn_df["churn"].values
+X = churn_df[["account_length", "customer_service_calls"]].values
+
+# Create a KNN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X, y)
+
+# %% 
+# Predict the labels for the X_new
+y_pred = knn.predict(X_new)
+
+# Print the predictions for X_new
+print(f"Predictions: {y_pred}")
+
+# %% 
+# Import the module
+from sklearn.model_selection import train_test_split
+
+X = churn_df.drop("churn", axis=1).values
+y = churn_df["churn"].values
+
+# Split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))

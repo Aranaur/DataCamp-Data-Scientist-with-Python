9ecdb170ea75c29995178a3f888eddd7232045b5
Ihor Miroshnychenko <ihor.miroshnychenko@kneu.ua>
2023-03-04 01:09:58 +0200

    29 fin

--- 29 - Hypothesis Testing in Python.py
+++ 29 - Hypothesis Testing in Python.py
@@ -341,3 +341,46 @@ gof_test = chisquare(f_obs=incoterm_counts['n'],
 print(gof_test)
 
 # %% 4. Non-Parametric Tests
+
+# Wilcoxon signed-rank test
+# Conduct a paired t-test on dem_percent_12 and dem_percent_16
+paired_test_results = pingouin.ttest(x=sample_dem_data['dem_percent_12'], 
+                                     y=sample_dem_data['dem_percent_16'],
+                                     paired=True,
+                                     alternative="two-sided")
+
+# Print paired t-test results
+print(paired_test_results)
+
+# Conduct a Wilcoxon test on dem_percent_12 and dem_percent_16
+wilcoxon_test_results = pingouin.wilcoxon(sample_dem_data['dem_percent_12'],sample_dem_data['dem_percent_16'])
+
+
+
+# Print Wilcoxon test results
+print(wilcoxon_test_results)
+
+# Non-parametric ANOVA and unpaired t-tests
+
+# Select the weight_kilograms and late columns
+weight_vs_late = late_shipments[["weight_kilograms", "late"]]
+
+# Convert weight_vs_late into wide format
+weight_vs_late_wide = weight_vs_late.pivot(columns='late', 
+                                           values='weight_kilograms')
+
+# Run a two-sided Wilcoxon-Mann-Whitney test on weight_kilograms vs. late
+wmw_test = pingouin.mwu(x=weight_vs_late_wide['No'],
+                        y=weight_vs_late_wide['Yes'],
+                        alternative='two-sided')
+
+# Print the test results
+print(wmw_test)
+
+# Run a Kruskal-Wallis test on weight_kilograms vs. shipment_mode
+kw_test = pingouin.kruskal(data=late_shipments, 
+                           dv='weight_kilograms',
+                           between='shipment_mode')
+
+# Print the results
+print(kw_test)
